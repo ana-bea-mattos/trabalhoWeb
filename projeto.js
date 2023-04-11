3ea8c6f5d60da3b21afdfa439b05c751d036f738
AnaBea <anabeatrizmattos98@gmail.com>
2023-04-10 22:29:30 -0300

    1.0 em produção

--- projeto.js
+++ projeto.js
@@ -1,8 +1,36 @@
-const meuPrimeiroApp = Vue.createApp({
-    data() {
-    return {
-    mensagemInicial: 'Bem-vindo ao VUE.JS',
+const meuPrimeiroCod= Vue.createCod({
+
+    data() { //variáveis
+        return {
+            msgInicial: 'Bem-vindo ao VUE.JS', 
+            verMsg: false,
+            corMsg: false,
+            contador: 0,
+            cores: [
+                { ptbr: 'AZUL' },
+                { ptbr: 'VERDE' },
+                { ptbr: 'VERMELHO' }
+            ],
+            msgText: "Para nada!"     //computed  
+        }
+    },
+
+
+    
+    //methods retorna um valor ligado ao alert
+    //computed tem que ligar a variável e o "this."
+    methods: {
+        avisoSistema(msgText) {
+            alert(this.msgText)  
+        }
     }
-    }
-   })
-   const mountedApp = meuPrimeiroApp.mount('#divPrincipal')
+})
+
+// criação do template para reutilizar os elementos criados
+meuPrimeiroApp.component('botao', {
+    template: `
+    <button>
+    Utilidade deste
+    </button> `
+})
+const mountedApp = meuPrimeiroCod.mount('#divPrincipal')
